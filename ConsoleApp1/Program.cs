f10d3547813d8a4bc24d75dc2c6686f9c29399b3
mrunal77 <mtmeshram77@outlook.com>
2023-08-15 17:40:27 +0530

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Reflection;
+
+Console.WriteLine("Hello, World!");
+
+string location, directoryPath;
+Assembly assembly = null;
+Type preProc = null;
+
+location = Assembly.GetEntryAssembly().Location;
+directoryPath = Path.GetDirectoryName(location);
+
+assembly = Assembly.LoadFrom(directoryPath + @"/TestClassLibrary.dll");
+preProc = assembly.GetType("TestClassLibrary.DemoClass");
+
+object obj = Activator.CreateInstance(preProc);
+
+MethodInfo preProcMethod = preProc.GetMethod("Sum");
+
+object[] methodparam = new object[2];
+methodparam[0] = 20;
+methodparam[1] = 30;
+
+var value = preProcMethod.Invoke(obj, methodparam);
+
+Console.WriteLine($"Value{value}");
+Console.ReadKey();
---
+++ TestClassLibrary/DemoClass.cs
@@ -0,0 +1,10 @@
+﻿namespace TestClassLibrary
+{
+    public class DemoClass
+    {
+        public int Sum(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
